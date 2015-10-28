6ffe6628be14df1bc7b01a0def91d96fb4548654
Chris Lewis <Bonsitm@gmail.com>
2015-10-27 23:28:16 -0700

    Reimplement old config page, bar themes

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -13,6 +13,28 @@ function debug(content) {
 
 /******************************** PebbleKit JS ********************************/
 
+Pebble.addEventListener('showConfiguration', function(e) {
+  Pebble.openURL('https://dl.dropboxusercontent.com/u/10824180/pebble%20config%20pages/beamup-basalt-configuration.html?version=' + VERSION);
+});
+
+Pebble.addEventListener('webviewclosed', function(e) {
+  var json = JSON.parse(decodeURIComponent(e.response));
+
+  var options = {
+    'DataKeyDate': '' + json.date,
+    'DataKeyAnimations': '' + json.animations,
+    'DataKeyBTIndicator': '' + json.bluetooth,
+    'DataKeyBatteryMeter': '' + json.battery,
+    'DataKeyHourlyVibration': '' + json.hourly
+  };
+
+  Pebble.sendAppMessage(options, function() {
+    console.log('Settings update successful!');
+  }, function(e) {
+    console.log('Settings update failed: ' + JSON.stringify(e));
+  });
+}); 
+
 Pebble.addEventListener('ready', function() {
   info('PebbleKit JS ready! Version ' + VERSION);
 });
--- src/modules/comm.c
+++ src/modules/comm.c
@@ -1,7 +1,34 @@
 #include "comm.h"
 
+// TODO Can collapse this, but it wouldn't be a good pattern to use
 static void inbox_received_handler(DictionaryIterator *iter, void *context) {
+  Tuple *date_tuple = dict_find(iter, DataKeyDate);
+  if(date_tuple) {
+    data_set_boolean_setting(DataKeyDate, strcmp(date_tuple->value->cstring, "true") == 0);
+  }
 
+  Tuple *anim_tuple = dict_find(iter, DataKeyAnimations);
+  if(anim_tuple) {
+    data_set_boolean_setting(DataKeyAnimations, strcmp(anim_tuple->value->cstring, "true") == 0);
+  }
+
+  Tuple *bt_tuple = dict_find(iter, DataKeyBTIndicator);
+  if(bt_tuple) {
+    data_set_boolean_setting(DataKeyBTIndicator, strcmp(bt_tuple->value->cstring, "true") == 0);
+  }
+
+  Tuple *battery_tuple = dict_find(iter, DataKeyBatteryMeter);
+  if(battery_tuple) {
+    data_set_boolean_setting(DataKeyBatteryMeter, strcmp(battery_tuple->value->cstring, "true") == 0);
+  }
+
+  Tuple *hourly_tuple = dict_find(iter, DataKeyHourlyVibration);
+  if(hourly_tuple) {
+    data_set_boolean_setting(DataKeyHourlyVibration, strcmp(hourly_tuple->value->cstring, "true") == 0);
+  }
+
+  // Exit to reload settings
+  window_stack_pop_all(true);
 }
 
 void comm_init(uint32_t inbox, uint32_t outbox, bool maximum) {
