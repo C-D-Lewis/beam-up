24e74638ab8dc9e34c9e5dae06aa527bb891b0f4
Chris Lewis <Bonsitm@gmail.com>
2015-11-12 00:22:12 -0800

    Don't point to testing config page

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -24,7 +24,7 @@ function debug(content) {
 /******************************** PebbleKit JS ********************************/
 
 Pebble.addEventListener('showConfiguration', function(e) {
-  Pebble.openURL('https://dl.dropboxusercontent.com/u/10824180/pebble%20config%20pages/beamup-basalt-configuration-testing.html?version=' + VERSION);
+  Pebble.openURL('https://dl.dropboxusercontent.com/u/10824180/pebble%20config%20pages/beamup-basalt-configuration.html?version=' + VERSION);
 });
 
 Pebble.addEventListener('webviewclosed', function(e) {
