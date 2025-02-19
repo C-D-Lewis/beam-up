3b0f83e774f0090a1a4918ebd4752269219f4696
C-D-Lewis <Bonsitm@gmail.com>
2025-02-19 20:28:04 +0000

    Upgrade project, fix Clay

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
 var VERSION = '3.6';
 
-var Clay = require('clay');
-var clayConfig = require('config.json');
+var Clay = require('pebble-clay');
+var clayConfig = require('./config.json');
 var clay = new Clay(clayConfig);
 
 Pebble.addEventListener('ready', function() {
